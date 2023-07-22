394885cb29969320e52dde17ed9d3970c5fee67d
Akas_Datta <012akasdatta@gmail.com>
2023-07-23 01:02:44 +0600

    navbar addded responsively

--- src/Pages/Home/Home/Shared/Navbar/Navbar.jsx
+++ src/Pages/Home/Home/Shared/Navbar/Navbar.jsx
@@ -1,87 +1,62 @@
-import './Navbar.css';
+import { useState } from 'react';
+import { IoMenu, IoClose } from 'react-icons/io5';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
-    <div className='bg-white'>
-        <div className="max-w-screen-xl mx-auto lg:px-10 overflow-x-hidden">
-        <header className="">
-            <div
-                className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8"
+    <div className='bg-white shadow'>
+        <nav className="p-3 md:flex md:items-center md:justify-between max-w-screen-xl mx-auto lg:px-10 overflow-x-hidden">
+        <div className="flex justify-between items-center">
+            <span className="text-2xl font-[Poppins] text-blue-600 font-bold cursor-pointer">
+            campusGo
+            </span>
+
+            <span
+            className="text-3xl cursor-pointer mx-2 md:hidden block"
+            onClick={toggleMenu}
             >
-                <a className="block text-blue-600" href="/">
-                {/* <span className="sr-only">Home</span> */}
-                <h2 className="font-bold text-2xl">CampusGo</h2>
-                </a>
-
-                <div className="flex flex-1 items-center justify-end md:justify-between">
-                <nav aria-label="Global" className="hidden md:block">
-                    <ul className="flex items-center lg:gap-7 md:gap-2 text-sm">
-
-                    <li>
-                        <a className="text-gray-500 transition hover:text-gray-500/75" href="/">
-                        Home
-                        </a>
-                    </li>
-
-                    <li>
-                        <a className="text-gray-500 transition hover:text-gray-500/75" href="/">
-                        Colleges
-                        </a>
-                    </li>
-
-                    <li>
-                        <a className="text-gray-500 transition hover:text-gray-500/75" href="/">
-                        Admission
-                        </a>
-                    </li>
-
-                    <li>
-                        <a className="text-gray-500 transition hover:text-gray-500/75" href="/">
-                        My College
-                        </a>
-                    </li>
-
-                    </ul>
-                </nav>
-
-                <div className="flex items-center gap-4">
-                    <div className="sm:flex sm:gap-4">
-                    <a>
-                        <input type="text" placeholder="Search" className="input input-bordered md:w-32 lg:w-42 bg-white" />
-                    </a>
-                    <a
-                        className="block rounded-md bg-blue-600 px-5 py-3 text-sm font-medium text-white transition hover:bg-blue-700"
-                        href="/"
-                    >
-                        Login
-                    </a>
-                    </div>
-
-                    <button
-                    className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden"
-                    >
-                    <span className="sr-only">Toggle menu</span>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                    >
-                        <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M4 6h16M4 12h16M4 18h16"
-                        />
-                    </svg>
-                    </button>
-                </div>
-                </div>
-            </div>
-            </header>
+            {menuOpen ? <IoClose /> : <IoMenu />}
+            </span>
         </div>
+
+        <ul
+            className={`md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 ${
+            menuOpen ? 'opacity-100 top-[80px]' : 'opacity-0 top-[-400px]'
+            } transition-all ease-in duration-500`}
+        >
+            <li className="mx-2 my-6 md:my-0">
+            <a href="#" className="text-lg hover:text-blue-500 duration-500">
+                Home
+            </a>
+            </li>
+            <li className="mx-2 my-6 md:my-0">
+            <a href="#" className="text-lg hover:text-blue-500 duration-500">
+                Colleges
+            </a>
+            </li>
+            <li className="mx-2 my-6 md:my-0">
+            <a href="#" className="text-lg hover:text-blue-500 duration-500">
+                Admission
+            </a>
+            </li>
+            <li className="mx-2 my-6 md:my-0">
+            <a href="#" className="text-lg hover:text-blue-500 duration-500">
+                My College
+            </a>
+            </li>
+            <div className="form-control mx-2 sm:y-5">
+                <input type="text" placeholder="Search" className="input input-bordered border-gray-300 bg-white md:w-36" />
+            </div>
+            <button className="bg-blue-600 text-white font-[Poppins] duration-500 px-6 py-2 hover:bg-blue-700 rounded">
+            Login
+            </button>
+        </ul>
+        </nav>
     </div>
   );
 };
