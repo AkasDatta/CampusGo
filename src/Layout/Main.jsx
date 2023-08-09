07935816101669f6b59a4c8e68500aa151233fdf
Akas_Datta <012akasdatta@gmail.com>
2023-08-09 20:05:14 +0600

    loader added and vercel problem solved

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,15 +1,27 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/Shared/Navbar/Navbar";
 import Footer from "../Pages/Shared/Footer/Footer";
-
-
+import { useEffect, useState } from "react";
 
 const Main = () => {
+    const [isVisible, setIsVisible] = useState(true);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+        setIsVisible(false);
+        }, 1000);
+        return () => clearTimeout(timeout);
+    }, []);
+    
     return (
         <div>
+            {isVisible ? <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-green-500"></div>
+      </div> : <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
+        </div> }
         </div>
     );
 };
